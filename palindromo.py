95ceacc8e2bc54e4745f3982c34fa8468f680e20
Jose Moris <moris.jose.jr@gmail.com>
2020-08-03 19:37:35 -0500

    archivo palindromo

---
+++ palindromo.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        from copy import deepcopy 
+        a = deepcopy(x)  
+        
+        if (x == 0):
+            return True
+        elif (x < 0) or (x % 10 == 0 ):
+            return False
+        
+        rev = 0
+        while (x != 0):
+            pop = x % 10
+            x //= 10
+            
+            rev = rev * 10 + pop
+          
+        if a == rev:
+            return True
+        else:
+            return False
+        
+
+n = Solution().isPalindrome(121)
+print(n)
